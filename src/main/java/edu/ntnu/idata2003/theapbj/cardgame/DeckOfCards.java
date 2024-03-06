227fd77a6bb9c6c92f3b511e23d7111b1b039017
theapbj <theabjartan@gmail.com>
2024-03-06 11:21:04 +0100

    Initial commit, edited pom, gitignore and created empty DeckOfCards class

---
+++ src/main/java/edu/ntnu/idata2003/theapbj/cardgame/DeckOfCards.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idata2003.theapbj.cardgame;
+
+
+/**
+ * Represents a deck of 52 cards.
+ *
+ * @author Thea Pernille Bjartan
+ * @version 2024-03-06
+ */
+
+public class DeckOfCards {
+
+}
